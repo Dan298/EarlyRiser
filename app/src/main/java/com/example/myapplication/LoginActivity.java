ce50953d37d1d7c4205f805dd775558104a8b1f2
Dan298 <dsg160030@utdallas.edu>
2019-02-23 15:10:48 -0600

    -password and email authentication

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -44,10 +44,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
@@ -191,13 +187,26 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        for(int count = 0; count < email.length(); count++)
+        {
+            if (email.charAt(count) == '@')
+            {
+                return email.contains("@");
+            }
+        }
+        return false;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        if (password.length() < 4)
+        {
+            return false;
+        }
+
+        else
+            {
+                return password.length() > 4;
+             }
     }
 
     /**
@@ -323,7 +332,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             }
 
-            // TODO: register the new account here.
+
             return true;
         }
 
