8837903cc5d466a11a7a9cd780676fd609652a87
Dan298 <dsg160030@utdallas.edu>
2019-02-23 19:52:35 -0600

    -Working

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,42 +1,45 @@
 package com.example.myapplication;
 import android.content.Intent;
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Username;
     private EditText Password;
     private TextView Info;
     private Button Login;
+    private Button Signup;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Username = (EditText) findViewById(R.id.Username);
+        Username = (EditText) findViewById(R.id.username);
         Password = (EditText) findViewById(R.id.password);
-        Info = (TextView) findViewById(R.id.Info);
+        Info = (TextView) findViewById(R.id.info);
         Login = (Button) findViewById(R.id.Login);
-        Login.setOnClickListener(new View.onClickListener() {
+        Login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                validate(Username.getText().toString(), Password.getText().toString());
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
             }
         });
+        Signup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-        private void validate(String Username, String Password)
-        {
-            if(Username == "Admin" && Password == "12345")
-            {
-                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
-            }
-        }
+
+
 
     }
 
